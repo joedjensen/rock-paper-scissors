6e2c87f5441bf615bf9c4787e7683f4dd72fb7e2
Priya B <priya_saran@yahoo.com>
2022-09-28 12:20:27 -0700

    added logic for decisions

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var wins=0;
-var loses=0;
+var losses=0;
 var ties=0;
 
+var choices = ['R', 'P', 'S'];
 do {
     do {
         userInput = prompt("Enter R, P, or S");
@@ -9,15 +10,47 @@ do {
     computerInput = generateComputerChoice();
     alert("Computer chooses " + computerInput);
     alert(generateResult(userInput, computerInput));
-    alert("stats");
+    alert("stats" + '\n' + "Wins: " + wins + '\n' + "Losses: " + losses + '\n' + "Ties: " + ties);
     var playAgain = confirm("play again");
 } while (playAgain)
 
 
 function generateComputerChoice(){
-    return "R"
+    var index = Math.floor(Math.random() * choices.length);
+    console.log("index: " + index);
+    return choices[index];
 }
 
 function generateResult(userInput, computerInput) {
+    switch (userInput) {
+        case 'R': switch (computerInput) {
+                    case 'R': reportTies();
+                    case 'P': reportLoss();
+                    case 'S': reportWins();
+                    };
+        case 'P': switch (computerInput) {
+                    case 'R': reportWins();
+                    case 'P': reportTies();
+                    case 'S': reportLoss();
+                    };
+        case 'S': switch (computerInput) {
+                    case 'R': reportLoss();
+                    case 'P': reportWins();
+                    case 'S': reportTies();
+                    };  
+    }
     return "You win!";
+}
+function reportLoss() {
+    losses++;
+    return "You lose!";
+}
+
+function reportWins() {
+    wins++;
+    return "You win!";
+}
+function reportTies() {
+    ties++;
+    return "You are tied!";
 }
